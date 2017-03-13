af7b7342d8787c3844da25579b1ec2c19348309b
Aswin Kallikadavil <Aswin Kallikadavil>
2017-03-14 01:08:45 +0530

    apiPOst method is added to service. Make it as observer and observable

--- src/app/api-communicator.service.ts
+++ src/app/api-communicator.service.ts
@@ -1,8 +1,32 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import { Http, Headers } from '@angular/http';
+import { Observable, Observer } from 'rxjs/Rx';
+import {environment} from '../environments/environment'
+
 
 @Injectable()
-export class ApiCommunicatorService {
+export class AppService {
+    constructor(private _http:Http){}
 
-  constructor() { }
+    apiPost(uri, data) {
+        let outputObserver: Observer<any>;
+        let url= environment.baseUrl;
+        let serverAddress= url+uri;
+        let output = new Observable(observer => outputObserver = observer).share();
+        data = JSON.stringify(data);
+        var headers = new Headers();
+        headers.append('Accept', 'application/json');
+        headers.append('Content-Type', 'application/json');
+            this._http.post(serverAddress, data, { headers: headers }).subscribe(res => {
+                let outputResponse = res.json();
+                  outputObserver.next(outputResponse);
+            }, (error) => {
+          
+                 outputObserver.error(error.status)
+            });
+            
+        return output;
+        
+    }
 
 }
