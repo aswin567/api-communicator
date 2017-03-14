8687d9214710eb396b310c1927e85a4a164db3b5
Aswin Kallikadavil <Aswin Kallikadavil>
2017-03-15 00:29:47 +0530

    New function added to the API-communicator service, namely apiGet. Used for get method API calls

--- src/app/api-communicator.service.ts
+++ src/app/api-communicator.service.ts
@@ -1,32 +1,47 @@
-import {Injectable} from '@angular/core';
+import { Injectable } from '@angular/core';
 import { Http, Headers } from '@angular/http';
 import { Observable, Observer } from 'rxjs/Rx';
-import {environment} from '../environments/environment'
+import { environment } from '../environments/environment'
 
 
 @Injectable()
 export class AppService {
-    constructor(private _http:Http){}
+  constructor(private _http: Http) { }
 
-    apiPost(uri, data) {
-        let outputObserver: Observer<any>;
-        let url= environment.baseUrl;
-        let serverAddress= url+uri;
-        let output = new Observable(observer => outputObserver = observer).share();
-        data = JSON.stringify(data);
-        var headers = new Headers();
-        headers.append('Accept', 'application/json');
-        headers.append('Content-Type', 'application/json');
-            this._http.post(serverAddress, data, { headers: headers }).subscribe(res => {
-                let outputResponse = res.json();
-                  outputObserver.next(outputResponse);
-            }, (error) => {
-          
-                 outputObserver.error(error.status)
-            });
-            
-        return output;
-        
-    }
+  apiPost(uri, data) {
+    let outputObserver: Observer<any>;
+    let url = environment.baseUrl;
+    let serverAddress = url + uri;
+    let output = new Observable(observer => outputObserver = observer).share();
+    data = JSON.stringify(data);
+    var headers = new Headers();
+    headers.append('Accept', 'application/json');
+    headers.append('Content-Type', 'application/json');
+    this._http.post(serverAddress, data, { headers: headers }).subscribe(res => {
+      let outputResponse = res.json();
+      outputObserver.next(outputResponse);
+    }, (error) => {
 
+      outputObserver.error(error.status)
+    });
+
+    return output;
+
+  }
+  apiGet(uri) {
+    let outputObserver: Observer<any>;
+    let url = environment.baseUrl;
+    let serverAddress = url + uri;
+    let output = new Observable(observer => outputObserver = observer).share();
+    this._http.get(uri).subscribe(res => {
+      let outputResponse = res.json();
+      outputObserver.next(outputResponse);
+    }, (error) => {
+
+      outputObserver.error(error.status)
+    });
+
+    return output;
+
+  }
 }
